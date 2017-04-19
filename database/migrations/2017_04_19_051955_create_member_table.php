16c49f4285dd525709d312b76fe374ee5468319b
Chengziqian <18663104463@163.com>
2017-04-19 13:41:40 +0800

    build migrations

---
+++ database/migrations/2017_04_19_051955_create_member_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->boolean('gender');
+            $table->date('birthday');
+            $table->string('school_number');
+            $table->string('qq');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('bank_number');
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+}
---
+++ database/migrations/2017_04_19_052012_create_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('member_id');
+            $table->date('date');
+            $table->string('action');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log');
+    }
+}
