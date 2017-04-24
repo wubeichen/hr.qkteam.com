b00377a2bfa45d6020737ea81f6b31ee733b4e5a
RSzyh <750332908@qq.com>
2017-04-24 20:45:36 +0800

    sbsb

--- resources/views/page/recruitment/item.blade.php
+++ resources/views/page/recruitment/item.blade.php
@@ -5,3 +5,55 @@
     route('member.import',[$recruitment->id])
 
 --}}
+
+@extends('index.basic')
+@section('main')
+    <div class="row">
+        <table class="table table-hover">
+            <caption>申请列表</caption>
+            <thead>
+                <tr>
+                <th>名字</th>
+                <th>性别</th>
+                <th>生日</th>
+                <th>学号</th>
+                <th>email</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($recruitments as $recruitment)
+                <tr>
+                <td>{{$recruiment->name}}</td>
+                <td>{{$recruiment->gender}}</td>
+                <td>{{$recruiment->birthday}}</td>
+                <td>{{$recruiment->school_number}}</td>
+                <td>{{$recruiment->email}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="row">
+        <table class="table table-hover">
+            <caption>申请列表</caption>
+            <thead>
+                <th>QQ</th>
+                <th>phone</th>
+                <th>个人简介</th>
+                <th>个人期望</th>
+                <th>技能</th>
+            </thead>
+            <tbody>
+            @foreach ($recruitments as $recruitment)
+                <tr>
+                <td>{{$recruiment->qq}}</td>
+                <td>{{$recruiment->phone}}</td>
+                <td>{{$recruiment->introduction}}</td>
+                <td>{{$recruiment->expectation}}</td>
+                <td>{{$recruiment->skill}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
--- resources/views/page/recruitment/success.blade.php
+++ resources/views/page/recruitment/success.blade.php
@@ -2,3 +2,10 @@
     print from posts successfully
 
 --}}
+
+@extends('index.basic')
+@section('main')
+<div>
+    <h2>报名成功！请耐心等待结果～～</h2>
+</div>
+@endsection
